6f7e952294148584e482d15c9e266b40893c1429
VitorDie <vitordie@protonmail.ch>
2025-12-11 15:30:33 -0300

    PayrollTest: testDeleteEmployee (test pass)

--- src/main/java/br/com/vitordie/payroll/DeleteEmployeeTransaction.java
+++ src/main/java/br/com/vitordie/payroll/DeleteEmployeeTransaction.java
@@ -1,4 +1,15 @@
 package br.com.vitordie.payroll;
 
-public class DeleteEmployeeTransaction {
+public class DeleteEmployeeTransaction extends Transaction {
+    private final int id;
+
+    public DeleteEmployeeTransaction(int id, PayrollDatabase database) {
+        super(database); // Chama o construtor da classe pai (Transaction)
+        this.id = id;
+    }
+
+    @Override
+    public void execute() {
+        database.deleteEmployee(id);
+    }
 }
