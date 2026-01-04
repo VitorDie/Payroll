781e6e8db12b15cff2e5e65ba14d7ece64db9038
VitorDie <vitordie@protonmail.ch>
2026-01-04 14:36:21 -0300

    PayrollTest: testSalariedUnionMemberDues (test pass)

--- src/test/java/br/com/vitordie/payroll/PayrollTest.java
+++ src/test/java/br/com/vitordie/payroll/PayrollTest.java
@@ -671,4 +671,35 @@ public class PayrollTest {
         Assertions.assertEquals(47.1, pc.getDeductions(), .001);
         Assertions.assertEquals(1000.0 - 47.1, pc.getNetPay(), .001);
     }
+
+    @Test
+    public void testHourlyUnionMemberServiceCharge() {
+        int empId = 1;
+        AddHourlyEmployee t = new AddHourlyEmployee(
+                empId, "Bill", "Home", 15.24, database);
+        t.execute();
+
+        int memberId = 7734;
+        ChangeMemberTransaction cmt = new ChangeMemberTransaction(empId, memberId, 9.42, database);
+        cmt.execute();
+
+        LocalDate payDate = LocalDate.of(2001, 11, 9);
+
+        ServiceChargeTransaction sct = new ServiceChargeTransaction(memberId, payDate, 19.42, database);
+        sct.execute();
+
+        TimeCardTransaction tct = new TimeCardTransaction(payDate, 8.0, empId, database);
+        tct.execute();
+
+        PaydayTransaction pt = new PaydayTransaction(payDate, database);
+        pt.execute();
+
+        Paycheck pc = pt.getPaycheck(empId);
+        Assertions.assertNotNull(pc);
+        Assertions.assertEquals(payDate, pc.getPayPeriodEndDate());
+        Assertions.assertEquals(8 * 15.24, pc.getGrossPay(), .001);
+        Assertions.assertEquals("Hold", pc.getField("Disposition"));
+        Assertions.assertEquals(9.42 + 19.42, pc.getDeductions(), .001);
+        Assertions.assertEquals((8 * 15.24) - (9.42 + 19.42), pc.getNetPay(), .001);
+    }
 }
