2f50d5286f7f99bf24401d37ba410f8ce1917bca
VitorDie <vitordie@protonmail.ch>
2026-01-04 14:14:06 -0300

    PayrollTest: testChangeUnaffiliatedMember (test pass)

--- src/main/java/br/com/vitordie/payroll/ChangeUnaffiliatedTransaction.java
+++ src/main/java/br/com/vitordie/payroll/ChangeUnaffiliatedTransaction.java
@@ -1,4 +1,23 @@
 package br.com.vitordie.payroll;
 
-public class ChangeUnaffiliatedTransaction {
+public class ChangeUnaffiliatedTransaction extends ChangeAffiliationTransaction {
+
+    public ChangeUnaffiliatedTransaction(int empId, PayrollDatabase database) {
+        super(empId, database);
+    }
+
+    @Override
+    protected Affiliation getAffiliation() {
+        return new NoAffiliation();
+    }
+
+    @Override
+    protected void recordMembership(Employee e) {
+        Affiliation affiliation = e.getAffiliation();
+        if (affiliation instanceof UnionAffiliation) {
+            UnionAffiliation unionAffiliation = (UnionAffiliation) affiliation;
+            int memberId = unionAffiliation.getMemberId();
+            database.removeUnionMember(memberId);
+        }
+    }
 }
